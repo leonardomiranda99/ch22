b3d8b898f09355fe49cfd7ec1a5b203ef9e09efc
Leonardo Miranda <leonardmiranda99@gmail.com>
2023-01-11 13:48:21 -0600

    modificaciones de jvascript

--- WEB07/js/js01/js01.js
+++ WEB07/js/js01/js01.js
@@ -1,1 +1,34 @@
-console.log("Sesión js01");
+console.log("Sesión js0102");
+/* son 7 datos primitivos e JavaSdcript
+-string ("hola mundo")
+-number (1800.22)
+    |los numeros se utilizan 64 bits para almanecarlo 
+    |pero, solo se usan 53 bits para representar el número
+-bigInt (1800n) 
+    |convertir un dato numerico a bigInt
+    |let bigIntVar = BigInt(maxNumber);
+    |bigIntVar +1n
+-boolean (true / false)
+-Undefined ( undefined )
+    |un tipo de dato no se define, solo se declara
+-Null
+    |intencionalmente se borra el tipo de dato.
+-Symbol
+
+tipos de datos object
+-IObject
+-Array
+
+Funciones
+
+
+*/
+let myObject ={clave: "valor", edad: 18, 5:5};
+console.log(myObject.edad);
+console.log(myObject['edad']);
+const propiedad ='edad';
+console.log(myObject[propiedad]);
+console.log(myObject['5']);
+
+
+
