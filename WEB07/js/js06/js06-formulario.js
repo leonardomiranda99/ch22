4f49b4c2addb727053e0078e86b58c158979066a
Leonardo Miranda <leonardmiranda99@gmail.com>
2023-01-19 14:26:32 -0600

    creacion de formularios

--- WEB07/js/js06/js06-formulario.js
+++ WEB07/js/js06/js06-formulario.js
@@ -1,1 +1,10 @@
-console.log('conectado')
+console.log('conectado');
+
+const refFormulario = document.forms['registro'];
+
+refFormulario.addEventListener('submit', (event)=>{
+    event.prevenDefault();
+    console.log('Evento submit activado')
+    const email = refFormulario.elements['inputEmail4'].value;
+    console.log(email);
+})
